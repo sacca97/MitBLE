5a244a827e0a3193a74a713c0404dfa0a1c92902
Joppe De Boe <r0852336@pc-klas1-6.esat.kuleuven.be>
2025-11-27 21:48:12 +0100

    Added parallel implementation for bruteforcer using OpenMP, also added a CMakeLists.txt to define a proper build

---
+++ python_cli/bruteforcer/build/CMakeFiles/bruteforcer_OpenMP.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bruteforcer_OpenMP.dir/bruteforcer_OpenMP.cpp.o"
+  "CMakeFiles/bruteforcer_OpenMP.dir/bruteforcer_OpenMP.cpp.o.d"
+  "bruteforcer_OpenMP"
+  "bruteforcer_OpenMP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/bruteforcer_OpenMP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
